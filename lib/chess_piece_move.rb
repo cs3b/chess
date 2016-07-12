872ef90e8813b6c304bb742aa806b1a2b507276f
Dominik Duda <gospodorz@gmail.com>
2016-07-12 09:57:20 +0200

    Moved CHARACTERS_MAP, #to_coordinate, #to_position, #to_number, #to_char to TransformPosition::Support

--- lib/chess_piece_move.rb
+++ lib/chess_piece_move.rb
@@ -3,8 +3,6 @@ require 'support/transform_position'
 ChessPieceMove = Struct.new(:position, :figure_move_class) do
   include Support::TransformPosition
 
-  CHARACTERS_MAP = ' abcdefgh'.freeze
-
   def possible_positions
     possible_coordinates(to_coordinate(position)).map do |coordinate|
       to_position(*coordinate)
@@ -12,22 +10,6 @@ ChessPieceMove = Struct.new(:position, :figure_move_class) do
   end
 
   private
-
-  def to_coordinate(position)
-    [to_number(position[0]), position[1].to_i]
-  end
-
-  def to_position(x, y)
-    to_char(x) + y.to_s
-  end
-
-  def to_number(char)
-    CHARACTERS_MAP.index(char)
-  end
-
-  def to_char(number)
-    CHARACTERS_MAP[number]
-  end
 
   def possible_coordinates(coordinate)
     figure_move_class.new(coordinate.first, coordinate.last).possible_coordinates
--- lib/support/transform_position.rb
+++ lib/support/transform_position.rb
@@ -1,5 +1,21 @@
 module Support
   module TransformPosition
+    CHARACTERS_MAP = ' abcdefgh'.freeze
 
+    def to_coordinate(position)
+      [to_number(position[0]), position[1].to_i]
+    end
+
+    def to_position(x, y)
+      to_char(x) + y.to_s
+    end
+
+    def to_number(char)
+      CHARACTERS_MAP.index(char)
+    end
+
+    def to_char(number)
+      CHARACTERS_MAP[number]
+    end
   end
 end
