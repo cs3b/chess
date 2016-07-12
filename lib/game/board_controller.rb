b6794a9d280ae55ed35438408e7d1ed510833f67
Robert <harezlak.robert7@gmail.com>
2016-07-12 14:39:57 +0200

    generate classes for figure_transform && board_contoller

---
+++ lib/game/board_controller.rb
@@ -0,0 +1,26 @@
+module Game
+  class BoardController
+    def initialize
+      @chess_board = {}
+    end
+
+    def self.chess_reset
+      new.tap do |chess_board|
+        chess_board[[1, 1]] = Figure::Rook.new(1, 1)
+        chess_board[[2, 1]] = Figure::Knight.new(2, 1)
+        chess_board[[3, 1]] = Figure::Bishop.new(3, 1)
+        chess_board[[4, 1]] = Figure::Queen.new(4, 1)
+        chess_board[[5, 1]] = Figure::King.new(5, 1)
+        chess_board[[6, 1]] = Figure::Bishop.new(6, 1)
+        chess_board[[7, 1]] = Figure::Knight.new(7, 1)
+        chess_board[[8, 1]] = Figure::Rook.new(8, 1)
+        for i in 1..8
+          chess_board[[i, 2]] = Figure::Pawn.new(i, 2)
+        end
+      end
+    end
+
+    def tap
+    end
+  end
+end
