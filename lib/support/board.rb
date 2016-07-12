64cf45e06d90b4f0354a2d9f77d4a6a0f7cff4b9
Robert <harezlak.robert7@gmail.com>
2016-07-12 10:22:30 +0200

    set const size of board

--- lib/support/board.rb
+++ lib/support/board.rb
@@ -1,7 +1,13 @@
 module Support
   class Board
+    SIZE = 8
+
     def on_board?(coord_x, coord_y)
-      (1..8).cover?(coord_x) && (1..8).cover?(coord_y)
+      range.cover?(coord_x) && range.cover?(coord_y)
+    end
+
+    def range
+      1..SIZE
     end
   end
 end
