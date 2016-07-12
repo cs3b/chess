abac3d53f7797db435f49842744a0e9cf2198ba8
dsznajder <damsznajder@gmail.com>
2016-07-12 14:04:15 +0200

    Rook class update

--- lib/figure/rook.rb
+++ lib/figure/rook.rb
@@ -2,17 +2,11 @@ require 'figure/base'
 
 module Figure
   class Rook < Base
-    def vectors
-      range.flat_map { |distance| directions.map { |direction| direction * distance }.each_slice(2).to_a }
-    end
-
-    protected
+    private
 
     def range
       (1..7)
     end
-
-    private
 
     def directions
       [NORTH_SOUTH, WEST_EAST].flatten
