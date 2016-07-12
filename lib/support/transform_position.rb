7fe6b65c7f13d025a96bc59b04dd999983ac2c6a
Damian Suchy <damian.kropka@gmail.com>
2016-07-12 09:33:24 +0200

    added Transform helpers

--- lib/support/transform_position.rb
+++ lib/support/transform_position.rb
@@ -1,5 +1,12 @@
 module Support
   module TransformPosition
+    ASCII_OFFSET = 96
+    def to_coordinate(position)
+      [position[0].ord - ASCII_OFFSET, position[1].to_i]
+    end
 
+    def to_position(coordinate)
+      [(coordinate.first + ASCII_OFFSET).chr, coordinate.last.to_s].join
+    end
   end
 end
