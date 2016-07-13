a3f6b63768e72588b4e7d9ecd509db34293fa25f
Robert <harezlak.robert7@gmail.com>
2016-07-13 10:21:13 +0200

    refactor in board_controller, attributes in methods

--- lib/game/board_controller.rb
+++ lib/game/board_controller.rb
@@ -6,40 +6,40 @@ module Game
 
     def self.white_reset
       new.tap do |chess_board|
-        add_figure(1, 1, Rook)
-        add_figure(2, 1, Knight)
-        add_figure(3, 1, Bishop)
-        add_figure(4, 1, Queen)
-        add_figure(5, 1, King)
-        add_figure(6, 1, Bishop)
-        add_figure(7, 1, Knight)
-        add_figure(8, 1, Rook)
-        8.times { |i| add_figure(i + 1, 2, Pawn) }
+        add_figure(1, 1, type: Rook)
+        add_figure(2, 1, type: Knight)
+        add_figure(3, 1, type: Bishop)
+        add_figure(4, 1, type: Queen)
+        add_figure(5, 1, type: King)
+        add_figure(6, 1, type: Bishop)
+        add_figure(7, 1, type: Knight)
+        add_figure(8, 1, type: Rook)
+        8.times { |i| add_figure(i + 1, 2, type: Pawn) }
       end
     end
 
     def self.black_reset
       new.tap do |chess_board|
-        add_figure(1, 8, 'black', Rook)
-        add_figure(2, 8, 'black', Knight)
-        add_figure(3, 8, 'black', Bishop)
-        add_figure(4, 8, 'black', King)
-        add_figure(5, 8, 'black', Queen)
-        add_figure(6, 8, 'black', Bishop)
-        add_figure(7, 8, 'black', Knight)
-        add_figure(8, 8, 'black', Rook)
-        8.times { |i| add_figure(i + 1, 7, 'black', Pawn) }
+        add_figure(1, 8, color: 'black', type: Rook)
+        add_figure(2, 8, color: 'black', type: Knight)
+        add_figure(3, 8, color: 'black', type: Bishop)
+        add_figure(4, 8, color: 'black', type: King)
+        add_figure(5, 8, color: 'black', type: Queen)
+        add_figure(6, 8, color: 'black', type: Bishop)
+        add_figure(7, 8, color: 'black', type: Knight)
+        add_figure(8, 8, color: 'black', type: Rook)
+        8.times { |i| add_figure(i + 1, 7, color: 'black', type: Pawn) }
       end
     end
 
     private
 
-    def add_figure(x, y, color, type)
-      chess_board[[x, y]] = figure(x, y, color, type)
+    def add_figure(x, y, attributes = {})
+      chess_board[[x, y]] = figure(x, y, attributes[:color] = attributes[:color] || 'white', attributes[:type])
     end
 
-    def figure(x, y, color, type)
-      figure_const(type).new(x, y, color)
+    def figure(x, y, attributes = {})
+      figure_const(attributes[:type]).new(x, y, attributes[:color] = attributes[:color] || 'white')
     end
 
     def figure_const(type)
