056160e13f5eb243e68792fc4a70245322a08e29
dsznajder <damsznajder@gmail.com>
2016-07-13 08:37:38 +0200

    board implementation, collision with other figures

--- lib/figure/base.rb
+++ lib/figure/base.rb
@@ -1,12 +1,10 @@
 require 'support/transform_position'
-require 'game/board'
 
 module Figure
   class Base
     attr_reader :x, :y, :color
 
     include Support::TransformPosition
-    include Game::Board
     NORTH_SOUTH = [[0, 1], [0, -1]].freeze
     WEST_EAST = [[-1, 0], [1, 0]].freeze
     N_EAST_S_WEST = [[-1, -1], [1, 1]].freeze
@@ -17,18 +15,14 @@ module Figure
       @color = color
     end
 
-    def possible_moves
-      moves.select { |move| on_board?(move.first, move.last) }
+    def moves
+      vectors.map { |vector_x, vector_y| [x + vector_x, y + vector_y] }
     end
 
     private
 
     def vectors
       range.flat_map { |distance| directions.map { |direction| direction * distance }.each_slice(2).to_a }
-    end
-
-    def moves
-      vectors.map { |vector_x, vector_y| [x + vector_x, y + vector_y] }
     end
   end
 end
--- lib/game/board.rb
+++ lib/game/board.rb
@@ -1,48 +1,59 @@
+require 'figure/knight'
+require 'figure/pawn'
+require 'figure/king'
+require 'figure/bishop'
+require 'figure/queen'
+require 'figure/rook'
+require 'figure/base'
+
 module Game
-  module Board
+  class Board
     SCOPE = (1..8)
-    def on_board?(x, y)
-      SCOPE.cover?(x) && SCOPE.cover?(y)
-    end
 
-    @chess_board = []
-
-    def start_statement
+    def initialize
+      @chess_board = {}
       start_statement_white
       start_statement_black
-      @chess_board
     end
 
     def start_statement_white
-      new.tap do |chess_board|
-        chess_board[1, 1] = Figure::Rook.new(1, 1, 'white')
-        chess_board[2, 1] = Figure::Knight.new(2, 1, 'white')
-        chess_board[3, 1] = Figure::Bishop.new(3, 1, 'white')
-        chess_board[4, 1] = Figure::Queen.new(4, 1, 'white')
-        chess_board[5, 1] = Figure::King.new(5, 1, 'white')
-        chess_board[6, 1] = Figure::Bishop.new(6, 1, 'white')
-        chess_board[7, 1] = Figure::Knight.new(7, 1, 'white')
-        chess_board[8, 1] = Figure::Rook.new(8, 1, 'white')
-        for i in 1..8
-          chess_board[[i, 2]] = Figure::Pawn.new(i, 2, 'white')
-        end
+      @chess_board[[1, 1]] = Figure::Rook.new([1, 1], 'white')
+      @chess_board[[2, 1]] = Figure::Knight.new([2, 1], 'white')
+      @chess_board[[3, 1]] = Figure::Bishop.new([3, 1], 'white')
+      @chess_board[[4, 1]] = Figure::Queen.new([4, 1], 'white')
+      @chess_board[[5, 1]] = Figure::King.new([5, 1], 'white')
+      @chess_board[[6, 1]] = Figure::Bishop.new([6, 1], 'white')
+      @chess_board[[7, 1]] = Figure::Knight.new([7, 1], 'white')
+      @chess_board[[8, 1]] = Figure::Rook.new([8, 1], 'white')
+      (1..8).each do |i|
+        @chess_board[[i, 2]] = Figure::Pawn.new([i, 2], 'white')
       end
     end
 
     def start_statement_black
-      new.tap do |chess_board|
-        chess_board[1, 8] = Figure::Rook.new(1, 8, 'black')
-        chess_board[2, 8] = Figure::Knight.new(2, 8, 'black')
-        chess_board[3, 8] = Figure::Bishop.new(3, 8, 'black')
-        chess_board[4, 8] = Figure::Queen.new(4, 8, 'black')
-        chess_board[5, 8] = Figure::King.new(5, 8, 'black')
-        chess_board[6, 8] = Figure::Bishop.new(6, 8, 'black')
-        chess_board[7, 8] = Figure::Knight.new(7, 8, 'black')
-        chess_board[8, 8] = Figure::Rook.new(8, 8, 'black')
-        for i in 1..8
-          chess_board[[i, 7]] = Figure::Pawn.new(i, 7, 'black')
-        end
+      @chess_board[[1, 8]] = Figure::Rook.new([1, 8], 'black')
+      @chess_board[[2, 8]] = Figure::Knight.new([2, 8], 'black')
+      @chess_board[[3, 8]] = Figure::Bishop.new([3, 8], 'black')
+      @chess_board[[4, 8]] = Figure::Queen.new([4, 8], 'black')
+      @chess_board[[5, 8]] = Figure::King.new([5, 8], 'black')
+      @chess_board[[6, 8]] = Figure::Bishop.new([6, 8], 'black')
+      @chess_board[[7, 8]] = Figure::Knight.new([7, 8], 'black')
+      @chess_board[[8, 8]] = Figure::Rook.new([8, 8], 'black')
+      (1..8).each do |i|
+        @chess_board[[i, 7]] = Figure::Pawn.new([i, 7], 'black')
       end
+    end
+
+    def valid_move?(move)
+      @chess_board[move].nil?
+    end
+
+    def possible_moves(x, y)
+      @chess_board[[x, y]].moves.select { |move| on_board?(move) && valid_move?(move) }
+    end
+
+    def on_board?(move)
+      SCOPE.cover?(move.first) && SCOPE.cover?(move.last)
     end
   end
 end
