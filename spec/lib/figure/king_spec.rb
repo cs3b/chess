1e2e964f1d6619e3c591c691b10ec947be07064f
Raskerov <nutscracker1610@gmail.com>
2016-07-13 15:30:43 +0200

    Created test for initial position  for Figure::King

--- spec/lib/figure/king_spec.rb
+++ spec/lib/figure/king_spec.rb
@@ -3,7 +3,10 @@ require 'figure/king'
 describe Figure::King do
   describe '#possible_coordinates' do
     context 'from initial position' do
-      pending 'returns 5 possible positions for next move'
+      let(:king) { Figure::King.new(4, 1, color: 'black') }
+      it 'returns 5 possible positions for next move' do
+        expect(king.possible_coordinates).to eql([[5, 2], [5, 1], [3, 1], [3, 2], [4, 2]])
+      end
     end
 
     context 'at least 1 place from border' do
