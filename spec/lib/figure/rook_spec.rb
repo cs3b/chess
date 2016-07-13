dc1fcb3ed018f0f0100150b850e38993be14f7fc
Raskerov <nutscracker1610@gmail.com>
2016-07-13 15:57:32 +0200

    Updated test for middle of board for Figure::Rook

--- spec/lib/figure/rook_spec.rb
+++ spec/lib/figure/rook_spec.rb
@@ -8,7 +8,7 @@ describe Figure::Rook do
     end
     context 'from middle of board' do
       let(:rook) { Figure::Rook.new(4, 4, color: 'white') }
-      it { expect(rook.possible_coordinates.count).to eql(14) }
+      it { expect(rook.possible_coordinates.sort).to eql([[1, 4], [2, 4], [3, 4], [4, 1], [4, 2], [4, 3], [4, 5], [4,6], [4, 7], [4, 8], [5, 4], [6, 4], [7, 4], [8, 4] ]) }
     end
   end
 end
