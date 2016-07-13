407f81c93a52488539630fbb4018b0257121a870
Raskerov <nutscracker1610@gmail.com>
2016-07-13 11:04:25 +0200

    Moved position converting behaviour to Support::TransformPosition

--- lib/chess_piece_move.rb
+++ lib/chess_piece_move.rb
@@ -2,8 +2,6 @@ require 'support/transform_position'
 
 ChessPieceMove = Struct.new(:position, :figure_move_class) do
   include Support::TransformPosition
-  ASCII_TRANSLATION = 96
-
   def possible_positions
     possible_coordinates(to_coordinate(position)).map do |coordinate|
       to_position(*coordinate)
@@ -14,13 +12,5 @@ ChessPieceMove = Struct.new(:position, :figure_move_class) do
 
   def possible_coordinates(coordinate)
     figure_move_class.new(coordinate.first, coordinate.last).possible_coordinates
-  end
-
-  def to_coordinate(position)
-    [position[0].ord - ASCII_TRANSLATION, position[1].to_i]
-  end
-
-  def to_position(x, y)
-    (x + ASCII_TRANSLATION).chr + y.to_s
   end
 end
