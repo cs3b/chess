7010e963ba3f1d2b083938ca285369c269f5a0cd
Raskerov <nutscracker1610@gmail.com>
2016-07-13 14:46:42 +0200

    Created test for initial place for Figure::Rook

---
+++ spec/lib/figure/pawn_spec.rb
@@ -0,0 +1,8 @@
+require 'figure/pawn'
+
+describe Figure::Pawn do
+  describe '#possible_coordinates' do
+    pending 'for starting place' # context tu
+    pending 'for any other place'
+  end
+end
--- spec/lib/figure/rook_spec.rb
+++ spec/lib/figure/rook_spec.rb
@@ -2,7 +2,10 @@ require 'figure/rook'
 
 describe Figure::Rook do
   describe '#possible_coordinates' do
-    pending 'from initial place'
+    context 'from initial place' do
+      let(:rook) { Figure::Rook.new(1, 1, color: 'black') }
+      it { expect(rook.possible_coordinates.count).to eql(14) }
+    end
     pending 'from middle of board'
   end
 end
