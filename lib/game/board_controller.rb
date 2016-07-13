9811dbf41ff43f659a118d5244f0a97ec8375668
Robert <harezlak.robert7@gmail.com>
2016-07-13 07:57:12 +0200

    changes in fetch_a_class

--- lib/game/board_controller.rb
+++ lib/game/board_controller.rb
@@ -22,14 +22,14 @@ module Game
 
     def self.black_reset
       new.tap do |chess_board|
-        chess_board[[1, 8]] = Figure::Rook.new(1, 8)
-        chess_board[[2, 8]] = Figure::Knight.new(2, 8)
-        chess_board[[3, 8]] = Figure::Bishop.new(3, 8)
-        chess_board[[4, 8]] = Figure::King.new(4, 8)
-        chess_board[[5, 8]] = Figure::Queen.new(5, 8)
-        chess_board[[6, 8]] = Figure::Bishop.new(6, 8)
-        chess_board[[7, 8]] = Figure::Knight.new(7, 8)
-        chess_board[[8, 8]] = Figure::Rook.new(8, 8)
+        # chess_board[[1, 8]] = Figure::Rook.new(1, 8)
+        # chess_board[[2, 8]] = Figure::Knight.new(2, 8)
+        # chess_board[[3, 8]] = Figure::Bishop.new(3, 8)
+        # chess_board[[4, 8]] = Figure::King.new(4, 8)
+        # chess_board[[5, 8]] = Figure::Queen.new(5, 8)
+        # chess_board[[6, 8]] = Figure::Bishop.new(6, 8)
+        # chess_board[[7, 8]] = Figure::Knight.new(7, 8)
+        # chess_board[[8, 8]] = Figure::Rook.new(8, 8)
       end
     end
 
