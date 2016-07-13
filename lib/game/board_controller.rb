6f4d3f5ea8f79700be297a3a64603e0e4ff7e805
Robert <harezlak.robert7@gmail.com>
2016-07-13 07:26:31 +0200

    board reset for black and white

--- lib/game/board_controller.rb
+++ lib/game/board_controller.rb
@@ -4,7 +4,7 @@ module Game
       @chess_board = {}
     end
 
-    def self.chess_reset
+    def self.white_reset
       new.tap do |chess_board|
         chess_board[[1, 1]] = Figure::Rook.new(1, 1)
         chess_board[[2, 1]] = Figure::Knight.new(2, 1)
@@ -17,6 +17,19 @@ module Game
         for i in 1..8
           chess_board[[i, 2]] = Figure::Pawn.new(i, 2)
         end
+      end
+    end
+
+    def self.black_reset
+      new.tap do |chess_board|
+        chess_board[[1, 8]] = Figure::Rook.new(1, 8)
+        chess_board[[2, 8]] = Figure::Knight.new(2, 8)
+        chess_board[[3, 8]] = Figure::Bishop.new(3, 8)
+        chess_board[[4, 8]] = Figure::King.new(4, 8)
+        chess_board[[5, 8]] = Figure::Queen.new(5, 8)
+        chess_board[[6, 8]] = Figure::Bishop.new(6, 8)
+        chess_board[[7, 8]] = Figure::Knight.new(7, 8)
+        chess_board[[8, 8]] = Figure::Rook.new(8, 8)
       end
     end
 
