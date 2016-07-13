183f4b3ceb7c72acd64e501f3747baa9cfdd09ff
Raskerov <nutscracker1610@gmail.com>
2016-07-13 14:59:29 +0200

    Created test for corner of board for Figure::Queen

--- spec/lib/figure/queen_spec.rb
+++ spec/lib/figure/queen_spec.rb
@@ -2,7 +2,10 @@ require 'figure/queen'
 
 describe Figure::Queen do
   describe '#possible_coordinates' do
-    pending 'in corner of board'
+    context 'in corner of board' do
+      let(:queen) { Figure::Queen.new(1, 1, color: 'white') }
+      it { expect(queen.possible_coordinates.count).to eql(21) }
+    end
     pending 'in the middle of the board'
   end
 end
