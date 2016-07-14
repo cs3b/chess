0854f30414e4f300ecd45b9287c6387ede244261
Dominik Duda <gospodorz@gmail.com>
2016-07-14 08:26:01 +0200

    Implemented king_spec, made context description in bishop_spec in #vectors more accurate

--- spec/lib/figure/king_spec.rb
+++ spec/lib/figure/king_spec.rb
@@ -1,13 +1,16 @@
 require 'figure/king'
 
 describe Figure::King do
-  describe '#possible_coordinates' do
-    context 'from initial position' do
-      pending 'returns 5 possible positions for next move'
-    end
+  let(:king) { described_class.new(starting_position) }
 
-    context 'at least 1 place from border' do
-      pending 'returns 8 possible positions for next move'
+  describe '#vectors' do
+    context 'in bottom left corner' do
+      let(:starting_position) { [1, 1] }
+      let(:king_moves) { [[1, 2], [2, 2], [2, 1], [2, 0], [1, 0], [0, 0], [0, 1], [0, 2]] }
+
+      it 'returns an array of not filtered coordinates' do
+        expect(king.moves).to match_array king_moves
+      end
     end
   end
 end
