4435dc2750a751b6fc68304cdb211778e2d30cf3
Raskerov <nutscracker1610@gmail.com>
2016-07-14 09:19:04 +0200

    Created test for at least one position from border for Figure::Bishop

--- spec/lib/figure/bishop_spec.rb
+++ spec/lib/figure/bishop_spec.rb
@@ -1,10 +1,12 @@
 require 'figure/bishop'
 
 describe Figure::Bishop do
-  let(:bishop) { described_class.new(starting_position) }
-
   describe '#possible_coordinates' do
     context 'at least 1 place from border' do
+      let(:bishop) { described_class.new(2, 2) }
+      it 'returns 9 position' do
+        expect(bishop.possible_coordinates.count).to eql(9)
+      end
     end
   end
 
