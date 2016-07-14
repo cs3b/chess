97c3013bad98d0c70208c7b342e8ef3dd32d5cde
Raskerov <nutscracker1610@gmail.com>
2016-07-14 09:22:07 +0200

    Created test for middle of board for Figure::Bishop

--- spec/lib/figure/bishop_spec.rb
+++ spec/lib/figure/bishop_spec.rb
@@ -4,12 +4,16 @@ describe Figure::Bishop do
   describe '#possible_coordinates' do
     context 'at least 1 place from border' do
       let(:bishop) { described_class.new(2, 2) }
-      it 'returns 9 position' do
+      it 'returns 9 positions' do
         expect(bishop.possible_coordinates.count).to eql(9)
       end
     end
   end
 
   context 'in the middle of the board' do
+    let(:bishop) { described_class.new(4, 4) }
+    it 'returns 14 positions' do
+      expect(bishop.possible_coordinates.count).to eql(13)
+    end
   end
 end
