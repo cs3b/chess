e760167cc0f21216c76b7910338d25f5cfc75e8a
Dominik Duda <gospodorz@gmail.com>
2016-07-14 08:05:23 +0200

    Implemented bishop_spec

--- spec/lib/figure/bishop_spec.rb
+++ spec/lib/figure/bishop_spec.rb
@@ -3,11 +3,18 @@ require 'figure/bishop'
 describe Figure::Bishop do
   let(:bishop) { described_class.new(starting_position) }
 
-  describe '#possible_coordinates' do
-    context 'at least 1 place from border' do
-    end
-  end
+  describe '#vectors' do
+    context 'one field away from the corner' do
+      let(:starting_position) { [2, 2] }
+      let(:bishop_moves) do
+        [[3, 3], [3, 1], [1, 1], [1, 3], [4, 4], [4, 0], [0, 0], [0, 4], [5, 5], [5, -1], [-1, -1], [-1, 5], [6, 6],
+         [6, -2], [-2, -2], [-2, 6], [7, 7], [7, -3], [-3, -3], [-3, 7], [8, 8], [8, -4], [-4, -4], [-4, 8], [9, 9],
+         [9, -5], [-5, -5], [-5, 9]]
+      end
 
-  context 'in the middle of the board' do
+      it 'returns an array of not filtered coordinates' do
+        expect(bishop.moves).to match_array bishop_moves
+      end
+    end
   end
 end
