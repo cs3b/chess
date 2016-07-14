f07a8b2bb946b9824687de7dbb6d1a9847b443a8
Damian Suchy <damian.kropka@gmail.com>
2016-07-14 10:44:27 +0200

    implemented MovesHelper spec

--- spec/lib/support/moves_helper_spec.rb
+++ spec/lib/support/moves_helper_spec.rb
@@ -1,4 +1,5 @@
 require 'support/moves_helper'
+require 'figure/rook'
 
 describe Support::MovesHelper do
   before do
@@ -8,17 +9,31 @@ describe Support::MovesHelper do
 
       def initialize
         @chess_board = {}
-        @chess_board[[4, 4]] = { x: 4, y: 4, color: 'white' }
-        @chess_board[[2, 4]] = { x: 2, y: 4, color: 'white' }
-        @chess_board[[4, 2]] = { x: 4, y: 2, color: 'black' }
+        @chess_board[[4, 4]] = Figure::Rook.new([4, 4], 'white')
+        @chess_board[[2, 4]] = Figure::Rook.new([2, 4], 'white')
+        @chess_board[[4, 2]] = Figure::Rook.new([4, 2], 'black')
       end
     end
   end
-  let(:moves_helper) { BoardDouble.new }
+  let(:board_double) { BoardDouble.new }
 
   describe '#possible_moves' do
+    let(:possible_moves) { [[4, 5], [4, 3], [3, 4], [5, 4], [4, 6], [4, 2], [6, 4], [4, 7], [4, 1], [1, 4], [7, 4], [4, 8], [8, 4]] }
+
     it 'returns array of possible moves' do
-      expect(moves_helper.possible_moves(4, 4)).to eql [[4, 3]]
+      expect(board_double.possible_moves(4, 4)).to match_array possible_moves
+    end
+  end
+
+  describe '#collision_check' do
+    it 'returns collision with another figure' do
+      expect(board_double.collision_check?([4, 4], [2, 4])).to be_falsey
+    end
+  end
+
+  describe '#figure_coords_after_move' do
+    it 'moves and returns new position' do
+      expect(board_double.figure_coords_after_move([4, 4], [4, 2])).to have_attributes(x: 4, y: 2, color: 'white')
     end
   end
 end
