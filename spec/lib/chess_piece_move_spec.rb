8dec6862a5c1ad556e22035ba7b58c54281e91ee
Raskerov <nutscracker1610@gmail.com>
2016-07-14 11:12:11 +0200

    Passed test for ChessPieceMove

--- spec/lib/chess_piece_move_spec.rb
+++ spec/lib/chess_piece_move_spec.rb
@@ -1,13 +1,13 @@
 require 'chess_piece_move'
 
 describe ChessPieceMove do
-  describe '#possible_positions', pending: true do
+  describe '#possible_positions' do
     context 'for g2' do
 
       let(:chess_piece_move) { described_class.new('g2', FigureMoveDouble) }
 
       before do
-        FigureMoveDouble = Struct.new(:coordinate) do
+        class FigureMoveDouble < Figure::Base
           def possible_coordinates
             [[2, 3], [3, 2]]
           end
